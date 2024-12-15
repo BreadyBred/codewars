928749f6e416eafdaab21834de9c18fd58dbad93
Romain <gerarromain@gmail.com>
2024-12-15 22:05:30 +0100

    Bug commits

---
+++ PHP/8kyu/Multiply.php
@@ -0,0 +1,11 @@
+<?php
+/*
+This code does not execute properly. Try to figure out why.
+function multiply(int $a, int $b): int {
+	$a * $b;
+}
+*/
+function multiply(int $a, int $b): int {
+	return $a * $b;
+}
+?>
